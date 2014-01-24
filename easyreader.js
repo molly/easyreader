a3fbf708a317942e57e06f7cd539b3719e0ef577
Molly White <molly.white5@gmail.com>
2014-01-24 16:12:24 -0500

    Add Ctrl-E keybinding to toggle Easyreader.

--- easyreader.js
+++ easyreader.js
@@ -1,9 +1,14 @@
 // Add Easyreader button
-$(document).ready( function () {
-	var erLink = mw.util.addPortletLink('p-personal', '#', 'Easyreader', 'pt-easyreader',
-	 'Reformat this page so it\'s easier to read', null, '#pt-logout');
+$( document ).ready( function () {
+	var erLink = mw.util.addPortletLink( 'p-personal', '#', 'Easyreader', 'pt-easyreader',
+		'Reformat this page so it\'s easier to read', null, '#pt-logout' );
 	$( erLink ).click( function ( e ) {
 		e.preventDefault();
-		$('html').toggleClass('easyreader');
+		$( 'html' ).toggleClass( 'easyreader' );
+	});
+	$(document).keydown( function e ) {
+		if ( e.keyCode==69 && e.ctrlKey ) {
+			$( 'html' ).toggleClass( 'easyreader' );
+		}
 	});
 });
