ccb82f6add99324f5690f1f42663310c7464a9b1
Molly White <molly.white5@gmail.com>
2014-01-24 16:19:26 -0500

    Change keyboard shortcut to Ctrl-Y.
    Apparently Chrome claims Ctrl-E...

--- easyreader.js
+++ easyreader.js
@@ -1,13 +1,17 @@
-// Add Easyreader button
+// Add Easyreader functionality
 $( document ).ready( function () {
+
+	// Clicking "Easyreader" in the bar in the top left toggles Easyreader
 	var erLink = mw.util.addPortletLink( 'p-personal', '#', 'Easyreader', 'pt-easyreader',
 		'Reformat this page so it\'s easier to read', null, '#pt-logout' );
 	$( erLink ).click( function ( e ) {
 		e.preventDefault();
 		$( 'html' ).toggleClass( 'easyreader' );
 	});
+
+	// Ctrl-Y toggles Eas[y]reader
 	$(document).keydown( function e ) {
-		if ( e.keyCode==69 && e.ctrlKey ) {
+		if ( e.keyCode==89 && e.ctrlKey ) {
 			$( 'html' ).toggleClass( 'easyreader' );
 		}
 	});
